0f5d67f8a0e63a63d58eccb1b5290cc848a857bb
Alex <alex.gabriel@mail.ru>
2020-03-03 19:43:17 +0300

    feat: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+
+    for (const bracket of str) {
+        const [startBracket, endBracket] = bracketsConfig.find(pair =>
+            pair.includes(bracket)
+        );
+        if (bracket === startBracket && !stack.includes(startBracket)) {
+            stack.push(endBracket);
+            continue;
+        }
+        const stackedBracket = stack.pop();
+        if (bracket === stackedBracket) {
+            continue;
+        }
+        return false;
+    }
+
+    return !stack.length;
+};
